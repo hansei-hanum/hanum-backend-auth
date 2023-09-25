4b5a62839e109d8800a6692a4456a00c324348e0
plkmnb100 <pcy06@kakao.com>
2023-09-25 19:21:29 +0900

    feat: add launch shell script

--- run-grpc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 grpc-main.py
--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-uvicorn main:app --host 0.0.0.0 --port 8000
+# 서버 타입에 따른 명령어 실행
+if [ "$SERVER_TYPE" == "HTTP" ]; then
+    gunicorn main:app --workers 8 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
+elif [ "$SERVER_TYPE" == "GRPC" ]; then
+    python3 grpc-main.py
+else
+    echo "Unknown SERVER_TYPE: $SERVER_TYPE"
+    exit 1
+fi
