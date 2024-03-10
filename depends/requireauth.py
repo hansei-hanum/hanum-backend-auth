f558c16f2906bc335f070be507f592a38d347da5
Soju06 <qlskssk@gmail.com>
2024-03-10 21:31:42 +0900

    fix: auth validation

--- depends/requireauth.py
+++ depends/requireauth.py
@@ -1,20 +1,20 @@
 from fastapi import Header
-from auth import AuthService
 from fastapi.exceptions import HTTPException
+
+from auth import AuthService
 
 
 async def RequireAuth(authorization: str = Header(...)):
-    # try:
-    #     token_type = authorization.split(" ")[0]
-    #     token = authorization.split(" ")[1]
+    try:
+        token_type = authorization.split(" ")[0]
+        token = authorization.split(" ")[1]
 
-    #     if token_type != "Bearer":
-    #         raise
+        if token_type != "Bearer":
+            raise
 
-    #     userid = await AuthService.authorize(token)
-    #     if not userid:
-    #         raise
-    #     return userid
-    # except:
-    #     raise HTTPException(status_code=401, detail="UNAUTHORIZED")
-    return 1
+        userid = await AuthService.authorize(token)
+        if not userid:
+            raise
+        return userid
+    except:
+        raise HTTPException(status_code=401, detail="UNAUTHORIZED")
